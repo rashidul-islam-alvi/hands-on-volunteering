4c0a0bc3d615dff7fcb30bb794894b98be02f03b
rai <rai@fiftytwodigital.com>
2025-03-11 14:55:57 +0600

    feat: add User and UserDto classes for user management

---
+++ server/server/Models/UserDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Models
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
