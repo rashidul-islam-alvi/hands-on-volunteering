948be674c98d800a794aa03d8952ba3071781fd2
rai <rai@fiftytwodigital.com>
2025-03-11 16:13:09 +0600

    feat: add RefreshTokenAsync method to IAuthServices

--- server/server/Services/IAuthServices.cs
+++ server/server/Services/IAuthServices.cs
@@ -7,5 +7,7 @@ namespace server.Services
     {
         Task<User?> RegisterAsync(UserDto request);
         Task<TokenResponseDto?> LoginAsync(UserDto request);
+
+        Task<TokenResponseDto?> RefreshTokenAsync(RefreshTokenRequestDto request);
     }
 }
