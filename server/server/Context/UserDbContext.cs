60b7d0d3201c61b28d06d1506470527de1b42dd1
rai <rai@fiftytwodigital.com>
2025-03-11 15:06:00 +0600

    feat: add database context

---
+++ server/server/Context/UserDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using server.Entities;
+using System.Collections.Generic;
+
+namespace server.Context
+{
+    public class UserDbContext(DbContextOptions<UserDbContext> options) : DbContext(options)
+
+    {
+        public DbSet<User> User { get; set; }
+    }
+}
