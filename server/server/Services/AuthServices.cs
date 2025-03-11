8207fbc8ecfc3b9e2997dc718ab974acd6a177b2
rai <rai@fiftytwodigital.com>
2025-03-11 15:23:45 +0600

    feat: implement register method to auth services

---
+++ server/server/Services/AuthServices.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using server.Context;
+using server.Entities;
+using server.Models;
+
+namespace server.Services
+{
+    public class AuthServices(UserDbContext context) : IAuthServices
+    {
+        public async Task<User?> RegisterAsync(UserDto request)
+        {
+
+            if (await context.User.AnyAsync(u => u.Username == request.Username))
+            {
+                return null;
+            }
+
+            var user = new User();
+            var hashedPassword = new PasswordHasher<User>().HashPassword(user, request.Password);
+            user.Username = request.Username;
+            user.Email = request.Email;
+            user.PasswordHash = hashedPassword;
+
+            context.User.Add(user);
+            await context.SaveChangesAsync();
+
+
+            return user;
+        }
+
+    }
+}
